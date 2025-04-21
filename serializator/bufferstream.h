c4fc5fce4def53e3ad68157653e3b06f0e82f1f0
cypok is not CbIpok <dmitrienkomy@gmail.com>
2025-04-21 21:38:16 +0700

    MSVC support

--- serializator/bufferstream.h
+++ serializator/bufferstream.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <variant>
 #include <type_traits>
@@ -12,17 +12,23 @@ using Id = uint64_t;
 using Buffer = std::vector<std::byte>;
 
 namespace detail {
-    template<typename T>
-    void writePrimitive(Buffer& buff, const T& val) {
-        const auto raw = reinterpret_cast<const std::byte*>(&val);
-        buff.insert(buff.end(), raw, raw + sizeof(T));
-    }
+	template<typename T>
+	void writePrimitive(Buffer& buff, const T& val) {
+		const auto raw = reinterpret_cast<const std::byte*>(&val);
+		buff.insert(buff.end(), raw, raw + sizeof(T));
+	}
 
-    template<typename T>
-    T readPrimitive(Buffer::const_iterator& it, Buffer::const_iterator) {
-        T v;
-        std::memcpy(&v, it.base(), sizeof(T));
-        it += sizeof(T);
-        return v;
-    }
+	template<typename T>
+	T readPrimitive(Buffer::const_iterator& it, Buffer::const_iterator) {
+		T v;
+		// copy sizeof(T) bytes from the byte‐buffer into v
+		std::memcpy(
+			static_cast<void*>(&v),
+			static_cast<const void*>(&*it),
+			sizeof(T)
+		);
+		it += sizeof(T);  // advance by that many bytes
+		return v;
+
+	}
 }
